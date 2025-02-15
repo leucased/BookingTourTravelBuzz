53f0e3ca44da1970f57aad3fc5cc22dc489f6f06
Pham Thi Doan Trang <leucas.contact@gmail.com>
2025-02-15 11:13:49 +0700

    ver 1.0

---
+++ BookingTourTravelBuzz/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using BookingTourTravelBuzz.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingTourTravelBuzz.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
