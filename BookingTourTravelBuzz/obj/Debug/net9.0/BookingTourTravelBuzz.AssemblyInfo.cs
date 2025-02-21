0735154c9290af10c554619cfacc44c9036ccb42
Pham Thi Doan Trang <leucas.contact@gmail.com>
2025-02-21 16:40:38 +0700

    cap nhat xong giao dien trang chu

--- BookingTourTravelBuzz/obj/Debug/net9.0/BookingTourTravelBuzz.AssemblyInfo.cs
+++ BookingTourTravelBuzz/obj/Debug/net9.0/BookingTourTravelBuzz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingTourTravelBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824c8d1e3433856d9841f5d724aa995ffd45cc39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93bd84d120b3d1a697525bf0f12180539ae8d585")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingTourTravelBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingTourTravelBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BookingTourTravelBuzz/wwwroot/js/site.js
+++ BookingTourTravelBuzz/wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+    function changeImage(imageSrc, tourName, clickedButton) {
+        document.getElementById("displayedImage").src = imageSrc;
 
-// Write your JavaScript code.
+    // Xóa class 'active' khỏi tất cả button
+    let buttons = document.querySelectorAll(".custom-nut");
+       buttons.forEach(button => button.classList.remove("active"));
+
+    // Thêm class 'active' cho button được chọn
+    clickedButton.classList.add("active");
+
+    // Cập nhật nội dung của infoBox
+    let tourData = {
+        "Phú Quốc": {
+        description: "Một địa điểm tuyệt vời với biển xanh và nắng vàng.",
+    duration: "3 ngày 2 đêm"
+           },
+    "Hạ Long": {
+        description: "Di sản thiên nhiên thế giới với hàng nghìn đảo đá vôi kỳ vĩ.",
+    duration: "2 ngày 1 đêm"
+           },
+    "Đà Nẵng": {
+        description: "Thành phố biển đáng sống nhất Việt Nam với nhiều cảnh đẹp.",
+    duration: "4 ngày 3 đêm"
+           },
+    "Đà Lạt": {
+        description: "Thành phố ngàn hoa với khí hậu mát mẻ quanh năm.",
+    duration: "3 ngày 2 đêm"
+           }
+       };
+
+    document.getElementById("tourTitle").innerText = tourName;
+    document.getElementById("tourDescription").innerText = tourData[tourName].description;
+    document.getElementById("tourDuration").innerText = tourData[tourName].duration;
+     }
